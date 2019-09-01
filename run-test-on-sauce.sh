af44b375af376f1716c5db504bbb2887f9ca5408
xcshanto <shanto906@gmail.com>
2019-09-01 17:03:56 +0600

    Updated-run-on-sauce.sh-file-part2

--- run-test-on-sauce.sh
+++ run-test-on-sauce.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env bash
+#!/bin/bash
 java -jar runner.jar espresso --test app-debug-androidTest.apk --app app-release.apk --username shanto96 --apikey CB3F4BDA83DA42D7B3A6B2CFB763B1E3 --datacenter US --device Samsung_Galaxy_S6_sjc_free
